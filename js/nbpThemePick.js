328a7b076b0d1276f93397deafb8b79aea9f55d1
NewsBoard Plugin Team <NewsBoard Plugin Team@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-04-13 19:44:19 +0000

    1.2.3
    git-svn-id: https://plugins.svn.wordpress.org/newsboard/trunk@892388 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/nbpThemePick.js
+++ js/nbpThemePick.js
@@ -1,92 +1,90 @@
-$nbp=jQuery.noConflict();
-
-(function ($nbp) {
-    $nbp.fn.extend({
-        nbpThemePick: function (options) {
-            $nbp('.theme .theme_lable .override_default').bind('click', function(){
-                var filename = $nbp(this).attr('id');
-                var obj;
-                var data = new Object();
-                var loader = 800;
-                $nbp('.theme .theme_lable input').each(function(){
-                    $nbp(this).attr('checked', false);
-                });
-                
-                $nbp('.custom .custom_lable input').attr('checked', true);
-                console.log(options); 
-                data.command = 'jsOverride';
-                data.plugin_dir = options.plugin_dir;
-                var dataString = JSON.stringify(data);
-                $nbp.post(options.plugin_dir + 'classes/nbpJsOverride.php', {data: dataString}, function (response) {
-                    obj = $nbp.parseJSON(response);
-                    
-                    
-                    $nbp.get(options.plugin_dir + 'admin/newsboard-appearance.tpl', function(data){
-                        for(var index in obj) 
-                        {
-                            var re = new RegExp("{" + index + "}","gi");
-                            data = data.replace( re, obj['' + index + ''] );
-                        }
-                        $nbp(this).nbpMsg(loader, 'Please wait...', options.plugin_dir);
-                        setTimeout(function(){
-                            $nbp('.nbp_settings_holder .appearance_settings').html(data);
-                        }, loader);
-p                    });
-                    
-                });
-            });
-            
-            $nbp('.nbp_settings_holder .theme_picker .left_arrow img').bind('click', function(){
-                $nbp(this).queue(function(){
-                    changeTheme(defSign('+'));
-                });
-            });
-            $nbp('.nbp_settings_holder .theme_picker .right_arrow img').bind('click', function(){
-                $nbp(this).queue(function(){
-                    changeTheme(defSign('-'));
-                });
-                
-            });
-            
-            function changeTheme(direction)
-            {
-                if(direction!='error')
-                    $nbp('.nbp_settings_holder .theme_slider').animate({ left: direction+'=' +options.theme_width }, options.speed, 'easeOutQuad', function(){
-                        $nbp('.nbp_settings_holder .theme_picker .right_arrow img').clearQueue();
-                        $nbp('.nbp_settings_holder .theme_picker .left_arrow img').clearQueue();
-                    });
-            }
-            
-            function defSign(ask)
-            {
-                if(options.hiddenThemes > 0)
-                {
-                    if($nbp('.nbp_settings_holder .theme_slider').cssNumber('left') < 0 && $nbp('.nbp_settings_holder .theme_slider').cssNumber('left') > options.hiddenThemes*options.theme_width*(-1))
-                        return ask;
-                    else if($nbp('.nbp_settings_holder .theme_slider').cssNumber('left') == 0)
-                    {
-                        if(ask == '-')
-                            return '-';
-                        else if(ask == '+')
-                            return 'error';
-                    }
-                    else if($nbp('.nbp_settings_holder .theme_slider').cssNumber('left') == options.hiddenThemes*options.theme_width*(-1))
-                    {
-                        if(ask == '-')
-                            return 'error';
-                        else if(ask == '+')
-                            return '+';
-                    }
-                }
-                else
-                    return 'error';
-            }
-    
-        }
-    });
-})($nbp);
-
-$nbp.fn.cssNumber = function(prop){
-    var v = parseInt(this.css(prop),10);
-    return isNaN(v) ? 0 : v;
+$nbp=jQuery.noConflict();
+(function ($nbp) {
+    $nbp.fn.extend({
+        nbpThemePick: function (options) {
+            $nbp('.theme .theme_lable .override_default').bind('click', function(){
+                var filename = $nbp(this).attr('id');
+                var obj;
+                var data = new Object();
+                var loader = 800;
+                $nbp('.theme .theme_lable input').each(function(){
+                    $nbp(this).attr('checked', false);
+                });
+                
+                $nbp('.custom .custom_lable input').attr('checked', true);
+                console.log(options); 
+                data.command = 'jsOverride';
+                data.plugin_dir = options.plugin_dir;
+                var dataString = JSON.stringify(data);
+                $nbp.post(options.plugin_dir + 'classes/nbpJsOverride.php', {data: dataString}, function (response) {
+                    obj = $nbp.parseJSON(response);
+                    
+                    
+                    $nbp.get(options.plugin_dir + 'admin/newsboard-appearance.tpl', function(data){
+                        for(var index in obj) 
+                        {
+                            var re = new RegExp("{" + index + "}","gi");
+                            data = data.replace( re, obj['' + index + ''] );
+                        }
+                        $nbp(this).nbpMsg(loader, 'Please wait...', options.plugin_dir);
+                        setTimeout(function(){
+                            $nbp('.nbp_settings_holder .appearance_settings').html(data);
+                        }, loader);
+p                    });
+                    
+                });
+            });
+            
+            $nbp('.nbp_settings_holder .theme_picker .left_arrow img').bind('click', function(){
+                $nbp(this).queue(function(){
+                    changeTheme(defSign('+'));
+                });
+            });
+            $nbp('.nbp_settings_holder .theme_picker .right_arrow img').bind('click', function(){
+                $nbp(this).queue(function(){
+                    changeTheme(defSign('-'));
+                });
+                
+            });
+            
+            function changeTheme(direction)
+            {
+                if(direction!='error')
+                    $nbp('.nbp_settings_holder .theme_slider').animate({ left: direction+'=' +options.theme_width }, options.speed, 'easeOutQuad', function(){
+                        $nbp('.nbp_settings_holder .theme_picker .right_arrow img').clearQueue();
+                        $nbp('.nbp_settings_holder .theme_picker .left_arrow img').clearQueue();
+                    });
+            }
+            
+            function defSign(ask)
+            {
+                if(options.hiddenThemes > 0)
+                {
+                    if($nbp('.nbp_settings_holder .theme_slider').cssNumber('left') < 0 && $nbp('.nbp_settings_holder .theme_slider').cssNumber('left') > options.hiddenThemes*options.theme_width*(-1))
+                        return ask;
+                    else if($nbp('.nbp_settings_holder .theme_slider').cssNumber('left') == 0)
+                    {
+                        if(ask == '-')
+                            return '-';
+                        else if(ask == '+')
+                            return 'error';
+                    }
+                    else if($nbp('.nbp_settings_holder .theme_slider').cssNumber('left') == options.hiddenThemes*options.theme_width*(-1))
+                    {
+                        if(ask == '-')
+                            return 'error';
+                        else if(ask == '+')
+                            return '+';
+                    }
+                }
+                else
+                    return 'error';
+            }
+    
+        }
+    });
+})($nbp);
+$nbp.fn.cssNumber = function(prop){
+    var v = parseInt(this.css(prop),10);
+    return isNaN(v) ? 0 : v;
 };
--- uninstall.php
+++ uninstall.php
@@ -1,9 +1,5 @@
-<?php
-if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )
-	exit ();
-
-//Delete plugin data that had been saved to WP DB
-delete_option('newsboard-settings');
-delete_option('newsboard-main');
-delete_option('newsboard-appearance');
+<?php
+if ( !defined( 'WP_UNINSTALL_PLUGIN' ) )	exit ();
+//Delete plugin data that had been saved to WP DB
+delete_option('newsboard-settings');delete_option('newsboard-main');delete_option('newsboard-appearance');
 ?>
